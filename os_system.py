34d10cbf68afb18c318a84c88dd9812552c0fd32
RYO623 <85973534+RYO623@users.noreply.github.com>
2022-11-30 15:36:12 +0900

    Add files via upload

---
+++ os_system.py
@@ -0,0 +1,21 @@
+import os
+
+URL="tcu-sdlab/shigi_research"
+DIR="shigi_research"
+FILE="aaa.txt"
+
+#git cloneコマンドを使用
+os.system('git clone https://github.com/%s.git'%URL)
+
+#ここに処理を記述
+
+os.system('chdir')
+
+#ディレクトリの移動用コマンド
+#os.chdir('..')
+
+#cloneしたプロジェクトを削除
+os.system('rmdir /s /q %s'%URL)
+
+#ファイルの削除
+#os.system('del /s /q %s.json'%FILE)
